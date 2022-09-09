8441081be1c6f2a3debf0f43a481f57a5863b9f1
withchristopher <chmaxwel@icloud.com>
2022-09-09 11:43:45 +0200

    updated package.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,10 @@ const path = require("path");
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addon-postcss'
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
